55ef2963487b2be438d4ecf777b1fef8f10328b8
Monroe Howard II <monroehowardii@Monroes-MacBook-Air.local>
2019-02-15 16:00:44 -0600

    update to appjs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="row">
+          <div className="col-md-4">
+            <p>Clicky Game</p>
+          </div>
+          <div className="col-md-4">
+            <p>Click an image to begin!</p>
+          </div>
+          <div classname="col-md-4">
+            <p>Score: <div id="score"></div> | Top Score: <div id="topScore"></div></p></div>
+        </div>
+          <div className="jumbotron">
+            <h1>Clicky Game!</h1>
+            <h4>Click on an image to earn points, but don't click on any more than once!</h4>
+          </div>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
         </header>
-      </div>
+      </div >
     );
   }
 }
