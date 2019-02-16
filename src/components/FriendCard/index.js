0510a2489dc397de5427fadb0f29cbbba3c3bfad
Monroe Howard II <monroehowardii@Monroes-MacBook-Air.local>
2019-02-16 16:12:41 -0600

    minor update

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function FriendCard(props) {
   return (
-    <div className="card">
+    <div className="card" onClick={() => props.setClick(props.id)}>
       <div className="img-container">
         <img alt={props.name} src={props.image} />
       </div>
@@ -20,7 +20,7 @@ function FriendCard(props) {
           </li>
         </ul>
       </div>
-      <span className="remove">𝘅</span>
+      <span onClick={() => props.removefriend(props.id)} className="remove">𝘅</span>
     </div>
   );
 }
